432300a1aa288344f22d1639c9642e4455441908
frankknoll <Knoll_Frank@web.de>
2022-03-01 17:59:41 +0100

    using JSON instead of CSV

--- docs/intensivstationen.js
+++ docs/intensivstationen.js
@@ -1,17 +1,6 @@
 function displayIntensiveCareCapacitiesChart(intensiveCareCapacitiesChartView, headingElement, kreisText, kreisValue) {
     headingElement.textContent = kreisText;
-    withCsvDo(
-        `data/intensivstationen/intensivstationen-${kreisValue}.csv`,
-        csv => intensiveCareCapacitiesChartView.displayChart({ data: csv, title: kreisText }));
-}
-
-function withCsvDo(file, csvConsumer) {
-    Papa.parse(
-        file,
-        {
-            header: true,
-            dynamicTyping: true,
-            download: true,
-            complete: results => csvConsumer(results.data)
-        });
+    fetch(`data/intensivstationen/intensivstationen-${kreisValue}.json`)
+        .then(response => response.json())
+        .then(json => intensiveCareCapacitiesChartView.displayChart({ data: json, title: kreisText }));
 }
