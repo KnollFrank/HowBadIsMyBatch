daf52bfa591573cadc91cb26990276f47c62c410
frankknoll <Knoll_Frank@web.de>
2022-03-10 15:18:18 +0100

    updating data

--- src/intensivstationen/intensivstationen.sh
+++ src/intensivstationen/intensivstationen.sh
@@ -3,5 +3,5 @@ PATH=/home/frankknoll/Dokumente/Corona/phantomjs-2.1.1-linux-x86_64/bin:/home/fr
 echo $PATH | mail -s "PATH" -r Knoll_Frank@web.de Knoll_Frank@web.de
 cd /home/frankknoll/Dokumente/Corona/projects/HowBadIsMyBatch-pages/src/intensivstationen
 jupyter nbconvert --to notebook --allow-errors --execute Intensivstationen.ipynb
-#jupyter nbconvert --to html Intensivstationen.nbconvert.ipynb
-#mailx -a 'Content-Type: text/html' -s "Intensivstationen" -r Knoll_Frank@web.de Knoll_Frank@web.de < Intensivstationen.nbconvert.html
+jupyter nbconvert --to html Intensivstationen.nbconvert.ipynb
+mailx -a 'Content-Type: text/html' -s "Intensivstationen" -r Knoll_Frank@web.de Knoll_Frank@web.de < Intensivstationen.nbconvert.html
