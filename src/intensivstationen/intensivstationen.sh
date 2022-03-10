0a803226ab524cc176fd361dad895406e14b5737
frankknoll <Knoll_Frank@web.de>
2022-03-10 14:00:02 +0100

    updating data

--- src/intensivstationen/intensivstationen.sh
+++ src/intensivstationen/intensivstationen.sh
@@ -3,4 +3,3 @@ cd /home/frankknoll/Dokumente/Corona/projects/HowBadIsMyBatch-pages/src/intensiv
 jupyter nbconvert --to notebook --allow-errors --execute Intensivstationen.ipynb
 jupyter nbconvert --to html Intensivstationen.nbconvert.ipynb
 mailx -a 'Content-Type: text/html' -s "Intensivstationen" -r Knoll_Frank@web.de Knoll_Frank@web.de < Intensivstationen.nbconvert.html
-
