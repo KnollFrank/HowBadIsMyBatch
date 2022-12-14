06601c9358ebca98b055fd9ecc9d0a448c5239c5
frankknoll <Knoll_Frank@web.de>
2022-12-15 00:35:00 +0100

    refactoring

--- src/intensivstationen/Chart.py
+++ src/intensivstationen/Chart.py
@@ -1,9 +1,17 @@
 from decouple import AutoConfig
 from Datawrapper import Datawrapper
+from ColumnsAdder import ColumnsAdder
+from KreiseReader import readKreise
+from MedianOfFreeBedsByKreisTableFactory import MedianOfFreeBedsByKreisTableFactory
 
-def createMedianOfFreeBedsByKreisChart(medianOfFreeBedsByKreisTable, chartTitle):
-    config = AutoConfig(search_path='../..')
-    dataWrapper = Datawrapper(config('DATAWRAPPER_API_TOKEN'))
+
+def createMedianOfFreeBedsByKreisChart(timeSeries, chartTitle):
+    dataWrapper = Datawrapper(AutoConfig(search_path='../..')('DATAWRAPPER_API_TOKEN'))
     dataWrapper.setChartTitle(chartTitle)
-    dataWrapper.uploadChartData(medianOfFreeBedsByKreisTable)
+    dataWrapper.uploadChartData(data = _createMedianOfFreeBedsByKreisTableForChoroplethMap(timeSeries))
     dataWrapper.publishChart()
+
+def _createMedianOfFreeBedsByKreisTableForChoroplethMap(timeSeries):
+    medianOfFreeBedsByKreisTableFactory = MedianOfFreeBedsByKreisTableFactory(timeSeries)
+    medianOfFreeBedsByKreisTable = medianOfFreeBedsByKreisTableFactory.createMedianOfFreeBedsByKreisTable('gemeindeschluessel').reset_index()
+    return ColumnsAdder(readKreise()).addKreisAndEinwohnerzahlColumns(medianOfFreeBedsByKreisTable)
