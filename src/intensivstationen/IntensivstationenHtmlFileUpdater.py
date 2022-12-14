eab69db9fd3de6f29e86745e97a13964719366a7
frankknoll <Knoll_Frank@web.de>
2022-12-14 22:13:59 +0100

    refactoring

--- src/intensivstationen/IntensivstationenHtmlFileUpdater.py
+++ src/intensivstationen/IntensivstationenHtmlFileUpdater.py
@@ -1,6 +1,18 @@
 from bs4 import BeautifulSoup
 from HtmlTransformerUtil import HtmlTransformerUtil
+from DateProvider import DateProvider
 from KreisOptionsSetter import KreisOptionsSetter
+from HtmlTransformerUtil import HtmlTransformerUtil
+
+
+def saveLastUpdatedIntensivstationen(lastUpdated, toHtmlFile):
+    def setLastUpdated(soup):
+        soup.find(id = "Datenstand").string.replace_with(lastUpdated.strftime(DateProvider.INTENSIVSTATIONEN_DATE_FORMAT))
+        return soup
+
+    HtmlTransformerUtil().applySoupTransformerToFile(
+        file = toHtmlFile,
+        soupTransformer = setLastUpdated)
 
 
 def saveKreisOptions(kreisOptions, toHtmlFile):
