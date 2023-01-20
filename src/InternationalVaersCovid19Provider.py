b046f99d99372de06445114e100276be2e481748
frankknoll <Knoll_Frank@web.de>
2023-01-20 10:18:03 +0100

    adding function get_international_VAERSVAX_VAERSSYMPTOMS_Covid19

--- src/InternationalVaersCovid19Provider.py
+++ src/InternationalVaersCovid19Provider.py
@@ -1,6 +1,7 @@
 from DataFrameFilter import DataFrameFilter
 import VaersReader
 import pandas as pd
+from VaersDescrReader import VaersDescrReader
 
 
 def getInternationalVaersCovid19(years):
@@ -11,3 +12,19 @@ def getInternationalVaersCovid19(years):
         ])
     internationalVaersCovid19 = DataFrameFilter().filterByCovid19(internationalVaers)
     return internationalVaersCovid19
+
+
+def get_international_VAERSVAX_VAERSSYMPTOMS_Covid19(years):
+    international_VAERSVAX, international_VAERSSYMPTOMS = _get_international_VAERSVAX_VAERSSYMPTOMSs(years)
+    international_VAERSVAX_Covid19 = DataFrameFilter().filterByCovid19(international_VAERSVAX)    
+    return international_VAERSVAX_Covid19, international_VAERSSYMPTOMS
+
+
+def _get_international_VAERSVAX_VAERSSYMPTOMSs(years):
+    vaersDescrReader = VaersDescrReader(dataDir = "VAERS")
+    internationalVaersDescrs = vaersDescrReader.readVaersDescrsForYears(years) + [vaersDescrReader.readNonDomesticVaersDescr()]
+    return _getVaersDescrByName(internationalVaersDescrs, 'VAERSVAX'), _getVaersDescrByName(internationalVaersDescrs, 'VAERSSYMPTOMS')
+
+
+def _getVaersDescrByName(vaersDescrs, vaersDescrName):
+    return pd.concat([vaersDescr[vaersDescrName] for vaersDescr in vaersDescrs])
