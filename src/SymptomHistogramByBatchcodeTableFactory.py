9bc06e368a3405d2f63b1c83eddd8fa0c655271f
frankknoll <Knoll_Frank@web.de>
2023-01-24 14:42:25 +0100

    formating

--- src/SymptomHistogramByBatchcodeTableFactory.py
+++ src/SymptomHistogramByBatchcodeTableFactory.py
@@ -2,7 +2,9 @@ class SymptomHistogramByBatchcodeTableFactory:
 
     @staticmethod
     def createSymptomHistogramByBatchcodeTable(symptomByBatchcodeTable):
-        return (symptomByBatchcodeTable
-            .groupby(symptomByBatchcodeTable.index.names)
-            ['SYMPTOM'].value_counts()
-            .to_frame(name = 'SYMPTOM_COUNT_BY_VAX_LOT'))
+        return (
+            symptomByBatchcodeTable
+                .groupby(symptomByBatchcodeTable.index.names)
+                ['SYMPTOM'].value_counts()
+                .to_frame(name = 'SYMPTOM_COUNT_BY_VAX_LOT')
+              )
