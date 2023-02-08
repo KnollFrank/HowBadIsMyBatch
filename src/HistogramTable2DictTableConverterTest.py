16e6c2a7291624abf937e3a4c6d78df0552bb649
frankknoll <Knoll_Frank@web.de>
2023-02-08 16:33:04 +0100

    refining HistogramTable2DictTableConverterTest

--- src/HistogramTable2DictTableConverterTest.py
+++ src/HistogramTable2DictTableConverterTest.py
@@ -9,10 +9,10 @@ class HistogramTable2DictTableConverterTest(unittest.TestCase):
     def test_convertHistogramTable2DictTable(self):
         # Given
         histogramTable = TestHelper.createDataFrame(
-                columns = ['SYMPTOM_COUNT_BY_VAX_LOT'],
-                data = [  [5],
-                          [1],
-                          [2]],
+                columns = ['SYMPTOM_COUNT_BY_VAX_LOT', 'COUNTRY'],
+                data = [  [5,                          'Germany'],
+                          [1,                          'Germany'],
+                          [2,                          'Russian Federation']],
                 index = pd.MultiIndex.from_tuples(
                     names =   ['VAX_LOT1', 'SYMPTOM'],
                     tuples = [['1808982',  'Blood pressure orthostatic abnormal'],
@@ -26,17 +26,19 @@ class HistogramTable2DictTableConverterTest(unittest.TestCase):
         assert_frame_equal(
             dictTable,
             TestHelper.createDataFrame(
-                columns = ['SYMPTOM_COUNT_BY_VAX_LOT'],
+                columns = ['SYMPTOM_COUNT_BY_VAX_LOT',                     'COUNTRY'],
                 data = [  [
                             {
                                 "Blood pressure orthostatic abnormal": 5,
                                 "Chest discomfort": 1
-                            }
+                            },
+                                                                           'Germany'
                           ],
                           [
                             {
                                 "Chest discomfort": 2
-                            }
+                            },
+                                                                           'Russian Federation'
                           ]],
                 index = pd.Index(
                     name =  'VAX_LOT1',
