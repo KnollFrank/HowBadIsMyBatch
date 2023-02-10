c80d570907539e7624b610eebfbac571fc93f798
frankknoll <Knoll_Frank@web.de>
2023-02-10 11:19:31 +0100

    adding method HistogramTable2DictTableConverter.convertGlobalHistogramTable2DictTable(globalHistogramTable)

--- src/HistogramTable2DictTableConverter.py
+++ src/HistogramTable2DictTableConverter.py
@@ -10,6 +10,13 @@ class HistogramTable2DictTableConverter:
                     [['SYMPTOM_COUNT_BY_VAX_LOT', 'COUNTRY']])
 
     @staticmethod
+    def convertGlobalHistogramTable2DictTable(globalSymptomHistogramByBatchcodeTable):
+        vax_lot_columns = globalSymptomHistogramByBatchcodeTable.index.names.difference(['SYMPTOM'])
+        return (globalSymptomHistogramByBatchcodeTable
+                    .groupby(vax_lot_columns)
+                    .agg(lambda histogram_with_vax_lots: HistogramTable2DictTableConverter._histogram_to_json(histogram_with_vax_lots, vax_lot_columns)))
+
+    @staticmethod
     def _histogram_to_json(histogram_with_vax_lots, vax_lot_columns):
         histogram = histogram_with_vax_lots.reset_index(level = vax_lot_columns, drop = True)
         return histogram.to_dict()
