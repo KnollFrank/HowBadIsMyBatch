8f1eb03d9b59c9599f913e1ce6bddb13f931ad8a
frankknoll <Knoll_Frank@web.de>
2023-02-10 08:30:41 +0100

    refactoring

--- src/MultiIndexValuesProviderTest.py
+++ src/MultiIndexValuesProviderTest.py
@@ -6,13 +6,16 @@ class MultiIndexValuesProviderTest(unittest.TestCase):
 
     def test_getValues(self):
         # Given
+        value1 = '1808982'
+        value2 = 'EW0175'
+        value3 = 'EW0176'
         multiIndex = pd.MultiIndex.from_tuples(
             names =   ['VAX_LOT1', 'VAX_LOT2'],
-            tuples = [['1808982',  'EW0175'],
-                      ['1808982',  'EW0176']])
+            tuples = [[value1,     value2],
+                      [value1,     value3]])
                         
         # When
         values = MultiIndexValuesProvider.getValues(multiIndex)
 
         # Then
-        self.assertEqual(values, {'1808982', 'EW0175', 'EW0176'})
+        self.assertEqual(values, {value1, value2, value3})
