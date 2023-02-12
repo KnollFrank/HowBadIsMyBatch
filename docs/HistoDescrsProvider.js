9aeab8956be5bc95696771ef4c152e20236f4715
frankknoll <Knoll_Frank@web.de>
2023-02-12 12:42:26 +0100

    saving global histograms to data/histograms/global folder

--- docs/HistoDescrsProvider.js
+++ docs/HistoDescrsProvider.js
@@ -1,7 +1,7 @@
 class HistoDescrsProvider {
 
     static getHistoDescrsForBatchcode(batchcode) {
-        return fetch(`data/histograms/${batchcode}.json`)
+        return fetch(`data/histograms/global/${batchcode}.json`)
             .then(response => response.json())
             .then(histoDescrs => {
                 histoDescrs.histograms.sort((histoDescr1, histoDescr2) => histoDescr1.batchcodes.length - histoDescr2.batchcodes.length);
