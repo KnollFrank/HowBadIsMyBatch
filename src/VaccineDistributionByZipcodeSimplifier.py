095f7f0b4c0842bf65a100f826f4fdc1a3a6e8ea
frankknoll <Knoll_Frank@web.de>
2023-03-10 17:52:07 +0100

    refactoring

--- src/VaccineDistributionByZipcodeSimplifier.py
+++ src/VaccineDistributionByZipcodeSimplifier.py
@@ -6,5 +6,5 @@ class VaccineDistributionByZipcodeSimplifier:
     def sumDoses(vaccineDistributionByZipcode):
         return (vaccineDistributionByZipcode
                 .groupby(['PROVIDER_NAME', 'ZIPCODE_SHP', 'LOT_NUMBER'])
-                .agg(DOSES_SHIPPED = pd.NamedAgg(column = 'DOSES_SHIPPED', aggfunc = 'sum'))
+                .agg(DOSES_SHIPPED = pd.NamedAgg(column = 'DOSES_SHIPPED', aggfunc = sum))
                 .reset_index())
