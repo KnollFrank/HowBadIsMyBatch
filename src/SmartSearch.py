ef9c026a699aedfa73d42b14cd567573d7ca8ed1
frankknoll <Knoll_Frank@web.de>
2023-03-23 18:12:10 +0100

    refactoring

--- src/SmartSearch.py
+++ src/SmartSearch.py
@@ -1,25 +1,11 @@
 import re
+from SmartRegexpFactory import SmartRegexpFactory
 
 
 class SmartSearch:
 
     def __init__(self, searchTerm):
-        self.regexp = _SmartRegexpFactory().createSmartRegexp(searchTerm)
+        self.smartRegexp = SmartRegexpFactory().createSmartRegexp(searchTerm)
 
     def matches(self, str):
-        return True if re.match(self.regexp, str, flags=re.IGNORECASE) else False
-
-
-class _SmartRegexpFactory:
-
-    def createSmartRegexp(self, searchTerm):
-        return rf'^{self.assertContainsWords(self.getWords(searchTerm))}.*$'
-
-    def getWords(self, searchTerm):
-        return re.split(r'\s+', searchTerm)
-
-    def assertContainsWords(self, words):
-        return ''.join([self.assertContainsWord(word) for word in words])
-
-    def assertContainsWord(self, word):
-        return f'(?=.*?{word})'
+        return True if self.smartRegexp.match(str) else False
