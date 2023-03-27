19e10454b9d149386f82ec9f8de3ab1c49383585
frankknoll <Knoll_Frank@web.de>
2023-03-27 22:45:15 +0200

    refactoring

--- src/CountriesColumnAdderTest.py
+++ src/CountriesColumnAdderTest.py
@@ -2,7 +2,7 @@ import unittest
 from TestHelper import TestHelper
 from pandas.testing import assert_frame_equal
 import pandas as pd
-from src.CountriesColumnAdder import CountriesColumnAdder
+from CountriesColumnAdder import CountriesColumnAdder
 
 class CountriesColumnAdderTest(unittest.TestCase):
     
