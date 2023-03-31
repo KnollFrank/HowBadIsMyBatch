85c5a0ee60068634dc2ef9cb0f725c1e51d203df
frankknoll <Knoll_Frank@web.de>
2023-03-31 12:40:05 +0200

    refining CountriesMergerTest

--- src/CountriesMerger.py
+++ src/CountriesMerger.py
@@ -7,7 +7,7 @@ class CountriesMerger:
     def mergeSrcIntoDst(src: pd.Series, dst: pd.Series):
         def merge(series):
             series = series.dropna()
-            return sorted(series[0].union(series[1:]))
+            return sorted(set().union(*series))
         return (pd
                 .merge(dst, src, how='left', left_index=True, right_index=True)
                 .apply(merge, axis='columns'))
--- src/CountriesMergerTest.py
+++ src/CountriesMergerTest.py
@@ -8,8 +8,8 @@ class CountriesMergerTest(unittest.TestCase):
 
     def test_mergeSrcIntoDst(self):
         # Given
-        dstCountries = pd.Series({'NO72A': {'CountryA-1'}, 'EW096': {'CountryA-2'}}, name='dstCountries')
-        srcCountries = pd.Series({'NO72A': 'CountryB-1',   'FS192': 'CountryB-2'}, name='srcCountries')
+        dstCountries = pd.Series({'NO72A': ['CountryA-1'], 'EW096': ['CountryA-2']}, name='dstCountries')
+        srcCountries = pd.Series({'NO72A': ['CountryB-1'], 'FS192': ['CountryB-2']}, name='srcCountries')
 
         # When
         mergedCountries = CountriesMerger.mergeSrcIntoDst(src = srcCountries, dst = dstCountries)
