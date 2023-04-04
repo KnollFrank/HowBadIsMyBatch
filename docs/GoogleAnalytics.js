7b753529f684576b6ca31c1bf79f0567049ad252
frankknoll <Knoll_Frank@web.de>
2023-04-04 12:29:12 +0200

    temporarily disabling GA events

--- docs/GoogleAnalytics.js
+++ docs/GoogleAnalytics.js
@@ -1,20 +1,22 @@
 class GoogleAnalytics {
 
     static click_batchcode(batchcode) {
-        gtag(
-            'event',
-            'click_batchcode',
-            {
-                'batchcode': batchcode
-            });
+        // FK-TODO: wieder aktivieren
+        // gtag(
+        //     'event',
+        //     'click_batchcode',
+        //     {
+        //         'batchcode': batchcode
+        //     });
     }
 
     static view_search_results(search_term) {
-        gtag(
-            'event',
-            'view_search_results',
-            {
-                'search_term': search_term
-            });
+        // FK-TODO: wieder aktivieren
+        // gtag(
+        //     'event',
+        //     'view_search_results',
+        //     {
+        //         'search_term': search_term
+        //     });
     }
 }
