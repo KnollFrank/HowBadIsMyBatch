9a7b04dd22a5d3434717fb49eca37414b885d901
frankknoll <Knoll_Frank@web.de>
2023-06-11 13:07:49 +0200

    merging bramch main into pages

--- docs/GoogleAnalytics.js
+++ docs/GoogleAnalytics.js
@@ -1,12 +1,11 @@
 class GoogleAnalytics {
 
     static click_batchcode(batchcode) {
-        // FK-FIXME: reactivate
-        // gtag(
-        //     'event',
-        //     'click_batchcode',
-        //     {
-        //         'batchcode': batchcode
-        //     });
+        gtag(
+            'event',
+            'click_batchcode',
+            {
+                'batchcode': batchcode
+            });
     }
 }
