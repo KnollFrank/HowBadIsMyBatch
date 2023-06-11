2b1e5016009c1428a016857207d5e52febdcd906
frankknoll <Knoll_Frank@web.de>
2023-06-11 10:45:31 +0200

    removing unused code

--- src/CountriesByBatchcodeProvider.py
+++ src/CountriesByBatchcodeProvider.py
@@ -1,7 +1,5 @@
 import pandas as pd
-from BatchCodeTableFactory import BatchCodeTableFactory
 from InternationalVaersCovid19Provider import getInternationalVaersCovid19
-from SummationTableFactory import SummationTableFactory
 from CountryCountsByBatchcodeTablesMerger import CountryCountsByBatchcodeTablesMerger
 
 
@@ -31,24 +29,6 @@ def _combineCountryCountsByBatchcodeTables(countryCountsByClickedBatchcode, coun
     for column in countryCountsByBatchcode.columns:
         countryCountsByBatchcode[column] = countryCountsByBatchcode[column].astype('int64')
     return countryCountsByBatchcode
-
-
-def getCountriesByClickedBatchcode():
-    return (CountryCountsByBatchcodeTablesMerger
-        .getCountryCountsByClickedBatchcodeTable()
-        .reset_index(level = 'COUNTRY')
-        .groupby('VAX_LOT')
-        .agg(
-            Countries =
-                pd.NamedAgg(
-                    column = 'COUNTRY',
-                    aggfunc = SummationTableFactory.sortCountries)))
-
-
-def getCountriesByBatchcodeBeforeDeletion():
-    internationalVaersCovid19 = getInternationalVaersCovid19(dataDir = 'VAERS/VAERSBeforeDeletion', years = [2020, 2021, 2022])
-    batchCodeTable = BatchCodeTableFactory(internationalVaersCovid19).createGlobalBatchCodeTable(countriesAsList = True)
-    return batchCodeTable[['Countries']]
 
 
 def filterByBatchcodes(countryCountsByBatchcode, batchcodes2Retain):
