f268bf28f869c615721073175372567ab5e8424a
frankknoll <Knoll_Frank@web.de>
2023-06-16 21:53:07 +0200

    adding last_updated to VaccineDistributionByZipcode.html

--- src/BatchCodeTableHtmlUpdater.py
+++ src/BatchCodeTableHtmlUpdater.py
@@ -5,29 +5,27 @@ from HtmlUtils import getBatchcodeOptions, getBatchcodes
 from DateProvider import DateProvider
 
 
-def updateBatchCodeTableHtmlFile(batchCodeTable, batchCodeTableHtmlFile):
+def updateBatchCodeTableHtmlFile(batchCodeTable, batchCodeTableHtmlFile, lastUpdated):
     batchcodeOptions = getBatchcodeOptions(getBatchcodes(batchCodeTable.sort_values(by = 'Adverse Reaction Reports', ascending = False)))
     _saveBatchcodeOptions(batchcodeOptions, batchCodeTableHtmlFile)
-    saveLastUpdatedBatchCodeTable(
-        DateProvider().getLastUpdatedDataSource(),
-        batchCodeTableHtmlFile)
+    saveLastUpdated2HtmlFile(lastUpdated, batchCodeTableHtmlFile)
 
 def _saveBatchcodeOptions(batchcodeOptions, batchCodeTableHtmlFile):
     HtmlTransformerUtil().applySoupTransformerToFile(
         file=batchCodeTableHtmlFile,
-        soupTransformer=lambda soup:
+        soupTransformer = lambda soup:
             BeautifulSoup(
                 BatchcodeOptionsSetter().setBatchcodeOptions(
                     html=str(soup),
                     options=batchcodeOptions),
                 'lxml'))
 
-def saveLastUpdatedBatchCodeTable(lastUpdated, batchCodeTableHtmlFile):
+def saveLastUpdated2HtmlFile(lastUpdated, htmlFile):
     def setLastUpdated(soup):
         soup.find(id="last_updated").string.replace_with(
             lastUpdated.strftime(DateProvider.DATE_FORMAT))
         return soup
 
     HtmlTransformerUtil().applySoupTransformerToFile(
-        file=batchCodeTableHtmlFile,
-        soupTransformer=setLastUpdated)
+        file = htmlFile,
+        soupTransformer = setLastUpdated)
