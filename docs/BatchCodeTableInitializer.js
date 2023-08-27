85550e6c6ec97c97e5e390cfbb76331e7ffd4cb3
frankknoll <Knoll_Frank@web.de>
2023-08-27 17:21:53 +0200

    refactoring

--- docs/BatchCodeTableInitializer.js
+++ docs/BatchCodeTableInitializer.js
@@ -62,17 +62,6 @@ class BatchCodeTableInitializer {
                                     this.#getColumnIndex('Company')
                                 ]
                         },
-                        // FK-TODO: make visible again
-                        {
-                            visible: false,
-                            targets:
-                                [
-                                    this.#getColumnIndex('Company'),
-                                    this.#getColumnIndex('Deaths'),
-                                    this.#getColumnIndex('Disabilities'),
-                                    this.#getColumnIndex('Life Threatening Illnesses')
-                                ]
-                        },
                         {
                             render: data => {
                                 const numberInPercent = parseFloat(data);
