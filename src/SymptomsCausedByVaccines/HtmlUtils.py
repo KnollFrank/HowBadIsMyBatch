83be262928d12763b49b4bf74aa0810440a32379
frankknoll <Knoll_Frank@web.de>
2023-10-10 11:12:09 +0200

    refactoring

--- src/SymptomsCausedByVaccines/HtmlUtils.py
+++ src/SymptomsCausedByVaccines/HtmlUtils.py
@@ -1,9 +1,13 @@
 def getVaccineOptions(vaccines):
-    return ['<option hidden disabled selected value>Select Vaccine</option>'] + _getOptions(vaccines)
+    return _getOptionsWithDefaultOption(defaultOptionText = 'Select Vaccine', values = vaccines)
 
 
 def getSymptomOptions(symptoms):
-    return ['<option hidden disabled selected value>Select Symptom</option>'] + _getOptions(symptoms)
+    return _getOptionsWithDefaultOption(defaultOptionText = 'Select Symptom', values = symptoms)
+
+
+def _getOptionsWithDefaultOption(defaultOptionText, values):
+    return ['<option hidden disabled selected value>{defaultOptionText}</option>'.format(defaultOptionText = defaultOptionText)] + _getOptions(values)
 
 
 def _getOptions(values):
