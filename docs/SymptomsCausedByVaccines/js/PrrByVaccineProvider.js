f28ee6e400b7d49c3ef33a6f245a991c1d63a523
frankknoll <Knoll_Frank@web.de>
2023-10-13 20:07:13 +0200

    making UI work again

--- docs/SymptomsCausedByVaccines/js/PrrByVaccineProvider.js
+++ docs/SymptomsCausedByVaccines/js/PrrByVaccineProvider.js
@@ -1,6 +1,6 @@
 class PrrByVaccineProvider {
 
     static getPrrByVaccine(symptom) {
-        return fetch(`../data/ProportionalReportingRatios/${symptom}.json`).then(response => response.json());
+        return fetch(`../data/ProportionalReportingRatios/symptoms/${symptom}.json`).then(response => response.json());
     }
 }
