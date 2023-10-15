08f258979c57d14e1b24fdf5cb17df509b29daa5
frankknoll <Knoll_Frank@web.de>
2023-10-15 12:23:31 +0200

    enabling and disabling downloadPrrByVaccineTableButton

--- docs/SymptomsCausedByVaccines/js/PrrByVaccineTableView.js
+++ docs/SymptomsCausedByVaccines/js/PrrByVaccineTableView.js
@@ -1,25 +1,34 @@
 class PrrByVaccineTableView {
 
     #prrByVaccineTable;
+    #downloadPrrByVaccineTableButton;
     #prrByVaccine;
     #symptom;
 
     constructor(prrByVaccineTableElement, downloadPrrByVaccineTableButton) {
         this.#prrByVaccineTable = new PrrByVaccineTable(prrByVaccineTableElement);
         this.#prrByVaccineTable.initialize();
-        downloadPrrByVaccineTableButton.addEventListener(
-            'click',
-            () => this.#downloadPrrByVaccine())
+        this.#initializeButton(downloadPrrByVaccineTableButton);
     }
 
     displayPrrByVaccineTable4Symptom(symptom) {
+        UIUtils.disableButton(this.#downloadPrrByVaccineTableButton);
         PrrByVaccineProvider
             .getPrrByVaccine(symptom)
             .then(prrByVaccine => {
                 this.#prrByVaccine = prrByVaccine;
                 this.#symptom = symptom;
                 this.#prrByVaccineTable.display(prrByVaccine);
+                UIUtils.enableButton(this.#downloadPrrByVaccineTableButton);
             });
+    }
+
+    #initializeButton(downloadPrrByVaccineTableButton) {
+        this.#downloadPrrByVaccineTableButton = downloadPrrByVaccineTableButton;
+        UIUtils.disableButton(downloadPrrByVaccineTableButton);
+        downloadPrrByVaccineTableButton.addEventListener(
+            'click',
+            () => this.#downloadPrrByVaccine())
     }
 
     #downloadPrrByVaccine() {
--- docs/UIUtils.js
+++ docs/UIUtils.js
@@ -1,5 +1,21 @@
 class UIUtils {
 
+    static show(element) {
+        element.style.display = "block";
+    }
+
+    static hide(element) {
+        element.style.display = "none";
+    }
+
+    static disableButton(button) {
+        button.disabled = true;
+    }
+
+    static enableButton(button) {
+        button.disabled = false;
+    }
+
     static instantiateTemplate(templateId) {
         return document.getElementById(templateId).content.firstElementChild.cloneNode(true);
     }
