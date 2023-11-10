998c55f50eac4a9d26577ec0a4e3c843d716cb26
frankknoll <Knoll_Frank@web.de>
2023-11-10 08:44:21 +0100

    refactoring

--- docs/SymptomsCausedByCOVIDLots/js/SymptomVsSymptomChartDataProvider.js
+++ docs/SymptomsCausedByCOVIDLots/js/SymptomVsSymptomChartDataProvider.js
@@ -1,17 +1,8 @@
 class SymptomVsSymptomChartDataProvider {
 
-    // FK-TODO: extract utility class and test
-    static retainCommonKeys({ dict1, dict2 }) {
-        const commonKeys = SymptomVsSymptomChartDataProvider.#getCommonKeys(dict1, dict2);
-        return {
-            dict1: SymptomVsSymptomChartDataProvider.#retainKeysOfDict(dict1, commonKeys),
-            dict2: SymptomVsSymptomChartDataProvider.#retainKeysOfDict(dict2, commonKeys)
-        };
-    }
-
     static getChartData({ prrByLotX, prrByLotY }) {
         const { dict1: prrByLotXCommon, dict2: prrByLotYCommon } =
-            SymptomVsSymptomChartDataProvider.retainCommonKeys(
+            DictUtils.retainCommonKeys(
                 {
                     dict1: prrByLotX,
                     dict2: prrByLotY
@@ -26,23 +17,5 @@ class SymptomVsSymptomChartDataProvider {
                         y: prrByLotYCommon[lot]
                     }))
         };
-    }
-
-    static #getCommonKeys(dict1, dict2) {
-        return Sets.intersection(
-            SymptomVsSymptomChartDataProvider.#getKeySet(dict1),
-            SymptomVsSymptomChartDataProvider.#getKeySet(dict2));
-    }
-
-    static #getKeySet(dict) {
-        return new Set(Object.keys(dict));
-    }
-
-    static #retainKeysOfDict(dict, keys2Retain) {
-        const entries2Retain =
-            Object
-                .entries(dict)
-                .filter(([key, _]) => keys2Retain.has(key));
-        return Object.fromEntries(entries2Retain);
     }
 }
--- docs/SymptomsCausedByCOVIDLots/test/SymptomVsSymptomChartDataProviderTest.js
+++ docs/SymptomsCausedByCOVIDLots/test/SymptomVsSymptomChartDataProviderTest.js
@@ -1,55 +1,5 @@
 QUnit.module('SymptomVsSymptomChartDataProviderTest', function () {
 
-    QUnit.test.each(
-        'shouldRetainCommonLots',
-        [
-            [
-                {
-                    dict1: {
-                        "lotX": 1.0
-                    },
-                    dict2: {
-                        "lotY": 2.0
-                    }
-                },
-                {
-                    dict1: {
-                    },
-                    dict2: {
-                    }
-                }
-            ],
-            [
-                {
-                    dict1: {
-                        "lotX": 1.0,
-                        "lotCommon": 2.0
-                    },
-                    dict2: {
-                        "lotCommon": 3.0,
-                        "lotY": 4.0
-                    }
-                },
-                {
-                    dict1: {
-                        "lotCommon": 2.0
-                    },
-                    dict2: {
-                        "lotCommon": 3.0
-                    }
-                }
-            ]
-        ],
-        (assert, [dicts, dictsHavingCommonKeys]) => {
-            // Given
-
-            // When
-            const dictsHavingCommonKeysActual = SymptomVsSymptomChartDataProvider.retainCommonKeys(dicts);
-
-            // Then
-            assert.deepEqual(dictsHavingCommonKeysActual, dictsHavingCommonKeys);
-        });
-
     QUnit.test('shouldProvideChartData', function (assert) {
         // Given
         const prrByLotX = {
