658076856d7e04412ec08170d47daf52cc64099e
Frank Knoll <Knoll_Frank@web.de>
2024-07-15 23:23:00 +0200

    adding headings

--- docs/SymptomsCausedByDrugs/js/PdfCreator.js
+++ docs/SymptomsCausedByDrugs/js/PdfCreator.js
@@ -3,8 +3,11 @@ class PdfCreator {
     static createPdf() {
         const documentDefinition = {
             content: [
-                { text: 'Customizing Page Size and Orientation', fontSize: 16, bold: true },
-                { text: 'This PDF has custom page size and orientation.' },
+                { text: 'EU Safety Signal (All drugs)', fontSize: 16, alignment: 'center', margin: [0, 0, 0, 20], bold: true },
+                { text: 'Worst Drugs', fontSize: 12, alignment: 'left', margin: [0, 10, 0, 10], bold: true },
+                { text: 'table' },
+                { text: 'Strongest Symptoms', fontSize: 12, alignment: 'left', margin: [0, 10, 0, 10], bold: true },
+                { text: 'table' },
             ]
         }
         return pdfMake.createPdf(documentDefinition);
