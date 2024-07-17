99fad75f547f5cc118e5c8a8d96ecfcd47091ff7
Frank Knoll <Knoll_Frank@web.de>
2024-07-17 10:11:52 +0200

    removing

--- docs/SymptomsCausedByDrugs/js/PrrByKeyTableView.js
+++ docs/SymptomsCausedByDrugs/js/PrrByKeyTableView.js
@@ -1,47 +1,21 @@
 class PrrByKeyTableView {
 
     #prrByKeyTable;
-    #downloadPrrByKeyTableButton;
-    #text;
-    #valueName;
     #prrByKeyProvider;
 
+    // FK-TODO: remove downloadPrrByKeyTableButton and valueName
     constructor(prrByKeyTable, downloadPrrByKeyTableButton, valueName, prrByKeyProvider) {
         this.#prrByKeyTable = prrByKeyTable;
         this.#prrByKeyTable.initialize();
-        this.#initializeButton(downloadPrrByKeyTableButton);
-        this.#valueName = valueName;
         this.#prrByKeyProvider = prrByKeyProvider;
     }
 
     displayPrrByKeyTable4Value(id, text) {
-        UIUtils.disableButton(this.#downloadPrrByKeyTableButton);
         this.#prrByKeyProvider(id)
-            .then(prrByKey => {
-                this.#text = text;
-                this.#prrByKeyTable.display(prrByKey);
-                UIUtils.enableButton(this.#downloadPrrByKeyTableButton);
-            });
+            .then(prrByKey => this.#prrByKeyTable.display(prrByKey));
     }
 
     getTable() {
         return this.#prrByKeyTable.getTable();
-    }
-
-    #initializeButton(downloadPrrByKeyTableButton) {
-        this.#downloadPrrByKeyTableButton = downloadPrrByKeyTableButton;
-        UIUtils.disableButton(downloadPrrByKeyTableButton);
-        downloadPrrByKeyTableButton.addEventListener(
-            'click',
-            () => this.#downloadPrrByKey())
-    }
-
-    #downloadPrrByKey() {
-        UrlUtils.downloadUrlAsFilename(
-            window.URL.createObjectURL(
-                new Blob(
-                    [this.#prrByKeyTable.getDisplayedTableAsCsv(`# ${this.#valueName}: ${this.#text}`)],
-                    { type: 'text/csv' })),
-            this.#text);
     }
 }
