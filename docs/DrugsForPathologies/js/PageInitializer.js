9e6be5c6eae035ed3b67674bdcba5d007838f54a
Frank Knoll <Knoll_Frank@web.de>
2024-07-28 11:13:40 +0200

    adding URLSearchParam

--- docs/DrugsForPathologies/js/PageInitializer.js
+++ docs/DrugsForPathologies/js/PageInitializer.js
@@ -1,20 +1,23 @@
 class PageInitializer {
 
-    static initializePage({ pathologySelectElement, drugsChartViewElement }) {
-        PageInitializer.#configurePathologySelect(pathologySelectElement, drugsChartViewElement);
+    static initializePage({ pathologySelectElement, drugsChartViewElement, urlSearchParam }) {
+        PageInitializer.#configurePathologySelect(pathologySelectElement, drugsChartViewElement, urlSearchParam);
     }
 
-    static #configurePathologySelect(pathologySelectElement, drugsChartViewElement) {
+    static #configurePathologySelect(pathologySelectElement, drugsChartViewElement, urlSearchParam) {
         const drugsChartView = new DrugsChartView(drugsChartViewElement);
         Select2.initializeSelectElement(
             {
                 selectElement: pathologySelectElement,
                 minimumInputLength: 0,
-                textOfOption2Select: null,
+                textOfOption2Select: urlSearchParam.get(),
                 onSelectOptionHavingValueAndText: (id, text) => {
                     fetch(`data/DrugDescriptionsForPathologies/${id}.json`)
                         .then(response => response.json())
-                        .then(drugDescr => drugsChartView.displayChart(drugDescr));
+                        .then(drugDescr => {
+                            drugsChartView.displayChart(drugDescr);
+                            urlSearchParam.set(text);
+                        });
                 }
             });
     }
