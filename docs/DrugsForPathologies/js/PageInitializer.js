02feabce8e629ebbb7e5d4ccbccbbff87c6b3aaa
Frank Knoll <Knoll_Frank@web.de>
2024-07-28 11:33:12 +0200

    refactoring

--- docs/DrugsForPathologies/js/PageInitializer.js
+++ docs/DrugsForPathologies/js/PageInitializer.js
@@ -1,24 +1,32 @@
 class PageInitializer {
 
     static initializePage({ pathologySelectElement, drugsChartViewElement, urlSearchParam }) {
-        PageInitializer.#configurePathologySelect(pathologySelectElement, drugsChartViewElement, urlSearchParam);
+        PageInitializer.#configurePathologySelect(
+            {
+                pathologySelectElement: pathologySelectElement,
+                drugsChartView: new DrugsChartView(drugsChartViewElement),
+                urlSearchParam: urlSearchParam
+            });
     }
 
-    static #configurePathologySelect(pathologySelectElement, drugsChartViewElement, urlSearchParam) {
-        const drugsChartView = new DrugsChartView(drugsChartViewElement);
+    static #configurePathologySelect({ pathologySelectElement, drugsChartView, urlSearchParam }) {
         Select2.initializeSelectElement(
             {
                 selectElement: pathologySelectElement,
                 minimumInputLength: 0,
                 textOfOption2Select: urlSearchParam.get(),
                 onSelectOptionHavingValueAndText: (id, text) => {
-                    fetch(`data/DrugDescriptionsForPathologies/${id}.json`)
-                        .then(response => response.json())
+                    PageInitializer
+                        .#loadDrugDescr(id)
                         .then(drugDescr => {
                             drugsChartView.displayChart(drugDescr);
                             urlSearchParam.set(text);
                         });
                 }
             });
+    }
+
+    static #loadDrugDescr(id) {
+        return fetch(`data/DrugDescriptionsForPathologies/${id}.json`).then(response => response.json());
     }
 }
